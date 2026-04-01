9549151891747aab9b4fcabea1444d920319fb20
Your Name <your@email.com>
2026-04-01 13:07:21 +0530

    Removed expo files and updated project

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './App.css';
